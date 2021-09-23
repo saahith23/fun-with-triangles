a5604b0c32fbd7623b7d0626305cd565068fbfbf
saahith23 <saahithvaspari23@gmail.com>
2021-09-23 20:43:27 +0530

    in

--- hypo.js
+++ hypo.js
@@ -2,7 +2,12 @@ const sides = document.querySelectorAll(".side-input");
 const hypotenuseBtn= document.querySelector("#hypotenuse-button");
 const outputE1= document.querySelector("#output");
 
+
+function calculateSumOfSquares(){
+    const sumOfSquares = a*a + b*b;
+    console.log(sumofSquares)
+}
 function calculateHypotenuse(){
-    console.log("hypotenuse");
+    calculateSumOfSquares(3,4);
 }
 hypotenuseBtn.addEventListener('click', calculateHypotenuse);
