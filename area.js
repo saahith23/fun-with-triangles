6b5528facbed0b9ea96aae51bb480da02c7d7adc
saahith23 <saahithvaspari23@gmail.com>
2021-09-25 18:21:28 +0530

    nav

---
+++ area.js
@@ -0,0 +1,20 @@
+var txtInput= document.querySelectorAll(".input-one");
+var btn = document.querySelector("#button");
+var txtOutput= document.querySelector("#output");
+
+function calculateProduct(a,b){
+    var product =a*b;
+    return product;
+
+}
+ function calculateArea(){
+     var product = calculateProduct(Number(txtInput[0].value), Number(txtInput[1].value));
+     var area = product/2;
+     
+     txtOutput.innerText = "the area of triangle is "+ area;
+ }
+
+
+
+
+btn.addEventListener('click', calculateArea);
